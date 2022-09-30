8bdf0670a303585d35fcf2e622565734a6eea0f6
Eugene Moshchyn <eugene.moschyn@gmail.com>
2022-09-30 14:33:31 -0700

    clean(): make main more readable

--- main.py
+++ main.py
@@ -1,96 +1,22 @@
 '''
     Author: Eugene Moshchyn
     Description:
-        This program scrapes all ZIP codes from
-        USPS Website
+        The script uses the beauty of the async programming to 
+        obtain Postal Code data via USPS API
 '''
+from dotenv import load_dotenv
 
-import json
-import asyncio
-import aiohttp
-from pathlib import Path
-from datetime import (datetime, timedelta)
-from navigation import (URL, HEADERS)
-import format
-
-
-async def get_page(session, zip):
-    '''
-        Gets the page information, keeping the logging
-    '''
-    data = {'zip': zip}
-    print(f'Sending Request for ZIP: {zip}')
-
-    async with session.post(URL['CITY_BY_ZIP'], data = data, headers = HEADERS, allow_redirects = False) as response:
-        print(f'Processing ZIP: {zip} --- Status Code: {response.status}')
-        return {zip: await response.json(content_type=None)}
-
-
-async def get_all_pages(session, zips):
-    '''
-        Gathers all the tasks
-    '''
-    tasks = [
-        asyncio.create_task( get_page(session, zip) )
-        for zip in zips
-    ]
-
-    results = await asyncio.gather(*tasks)
-    return results
-
-
-async def response(zips):
-    '''
-        Creates the async session
-    '''
-    async with aiohttp.ClientSession() as session:
-        data = await get_all_pages(session, zips)
-
-    return data
-
-
-def _get_zip_code_data():
-    # Run all 5 digit ZIP codes
-    zip_list = [f'{zip:05d}' for zip in range(100_000)]
-    data = asyncio.run( response(zip_list) )
-
-    # Chache the file
-    with open('data.json', 'w') as fout:
-        json.dump(data, fout, indent = 4)
-
-    return format.format_json( Path('data.json') )
-
-
-def _file_is_good(file_in: Path):
-    return (
-        ( file_in.is_file() ) and
-        ( timedelta(hours = 12) > ( datetime.now() - datetime.fromtimestamp(file_in.stat().st_mtime)) )
-    )
-
-
-def get_data():
-    fin = Path('formatted.json')
-    if ( _file_is_good(fin) ):
-        return json.load( fin.open('r') )
-
-    fin = Path('data.json')
-    if ( _file_is_good(fin) ):
-        return format.format_json(fin)
-
-    return _get_zip_code_data()
+from config import config
+from scraper import get_postal_codes
 
 
 def main():
-    # Format of ZIP codes: XXYYY => Total Number = 100,000
-    data = get_data()
+    load_dotenv()
 
-    for city, zip in data.items():
-        print(f'{city} - {zip}')
+    config()
 
-    print()
-    print(f'First ZIP: {list(data.values())[0]}')
-    print(f'Last  ZIP: {list(data.values())[-1]}')
-    print(f'Total Num of ZIPs: {len(data.keys())}')
+    data = get_postal_codes()
+
 
 if __name__ == '__main__':
     try:
